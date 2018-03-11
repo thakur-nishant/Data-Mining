5210d5917ec345db601c4f22b899b724b1fac9a0
Nishant Thakur <nt.nishantt@gmail.com>
2018-03-11 18:37:25 -0500

    made changes to the plot graph
    in Python

--- CentroidClassification/PlotAccuracy.py
+++ CentroidClassification/PlotAccuracy.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import random
 
 import CentroidClassification as cc
 
@@ -8,21 +9,27 @@ def run():
     filename = "HandWrittenLetters.txt"
 
     # class_ids = [1,2,3,4,5,6,7,8,9,10]
-    # class_ids = random.sample(range(1, 27), 5)
-    class_ids = [x for x in range(11, 21)]
+    class_ids = random.sample(range(1, 27), 10)
+    # class_ids = [x for x in range(11, 21)]
     print("For class:", class_ids)
 
     n = 38
     i = 5
+    x_axis = []
+    y_axis = []
     while i < n:
         test_instances = [i, n]
         print("\n############################")
         print("When train =", i, "and test =", n - i + 1)
         accuracy = cc.start(filename, class_ids, test_instances)
-        plt.scatter(i, accuracy, s=80, c="b", marker="x")
+        x_axis.append(i)
+        y_axis.append(accuracy)
+        # plt.scatter(i, accuracy, s=80, c="b", marker="x")
         i += 5
 
+    plt.plot(x_axis, y_axis)
     plt.show()
 
 
-run()
+if __name__ == "__main__":
+    run()
