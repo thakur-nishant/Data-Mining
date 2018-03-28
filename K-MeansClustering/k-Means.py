07ced5f27149496cdccacfee55b115721f039ced
Nishant Thakur <nt.nishantt@gmail.com>
2018-03-27 21:56:05 -0500

    added a script to generate the output required for the assignment
    script to generate output file

--- K-MeansClustering/k-Means.py
+++ K-MeansClustering/k-Means.py
@@ -1,23 +1,64 @@
-from sklearn.cluster import KMeans
+import numpy as np
+import random
 from sklearn.metrics import confusion_matrix
 from sklearn.utils.linear_assignment_ import linear_assignment
-import numpy as np
+
+
+# Function to calculate the Euclidean distance between 2 points
+# x & y : vectors representing 2 points
+def euclidean_distance(x, y):
+    sum = 0
+    for i in range(len(x)):
+        sum += (float(x[i]) - float(y[i]))**2
+
+    return np.sqrt(sum)
+
+
+def nearest_centroid(centroids, x):
+    distance = [euclidean_distance(mean, x) for mean in centroids]
+    nearest_centroid_index = distance.index(min(distance))
+
+    return nearest_centroid_index
+
+
+def form_cluster(X, k):
+    cluster_centroid = random.sample(X,k)
+    while True:
+        Y = []
+        prev_cluster_centroid = cluster_centroid
+        new_cluster = [[] for i in range(k)]
+        for point in X:
+            nearest_centroid_index = nearest_centroid(cluster_centroid, point)
+            new_cluster[nearest_centroid_index].append(point)
+            Y.append(nearest_centroid_index)
+
+        for i,cluster in enumerate(new_cluster):
+            sum = np.array([0 for j in cluster[0]],dtype='float')
+            for point in cluster:
+                sum += np.array(point, dtype='float')
+
+            cluster_centroid[i] = sum/len(cluster)
+
+        if cluster_centroid == prev_cluster_centroid:
+            break
+
+    return Y
 
 
 def start(filename, k):
-    train = []
+    data = []
+
     with open(filename) as f:
         for line in f:
-            data = line[:-1].split(',')
-            train.append(data)
+            row = line[:-1].split(',')
+            data.append(row)
 
-    Y = np.array(train[0], dtype='int')
-    X = np.array(train[1:]).transpose().tolist()
-    kmeans = KMeans(n_clusters=k)
-    kmeans.fit(X)
-    Kmeans_labels = kmeans.labels_
+    Y = np.array(data[0], dtype='int')
+    X = np.array(data[1:]).transpose().tolist()
 
-    C = confusion_matrix(Y, Kmeans_labels)
+    predicted_labels = form_cluster(X, k)
+
+    C = confusion_matrix(Y, predicted_labels)
     C = C.T
     ind = linear_assignment(-C)
     C_opt = C[:, ind[:, 1]]
@@ -25,23 +66,8 @@ def start(filename, k):
     print(acc_opt)
 
 
-if __name__ == "__main__":
-    # filename = "HandWrittenLetters.txt"
+if __name__ == '__main__':
+    # filename = 'ATNTFaceImages400.txt'
     filename = "ATNTFaceImages100.txt"
-    # train = []
-    # with open(filename) as f:
-    #     for line in f:
-    #         data = line[:-1].split(',')
-    #         train.append(data[:100])
-    # data = ""
-    # for i in range(len(train)):
-    #     string = ""
-    #     for j in train[i]:
-    #         string += str(j) + ","
-    #     data += string[:-1] + "\n"
-    #
-    # with open('ATNTFaceImages100.txt', 'w') as f:
-    #     f.write(data)
-    # f.close()
     k = 10
     start(filename, k)
--- K-MeansClustering/kMeans.py
+++ K-MeansClustering/kMeans.py
@@ -1,75 +1,54 @@
-import numpy as np
-import random
+from sklearn.cluster import KMeans
 from sklearn.metrics import confusion_matrix
 from sklearn.utils.linear_assignment_ import linear_assignment
+import numpy as np
+np.set_printoptions(threshold=np.nan)
 
-
-# Function to calculate the Euclidean distance between 2 points
-# x & y : vectors representing 2 points
-def euclidean_distance(x, y):
-    sum = 0
-    for i in range(len(x)):
-        sum += (float(x[i]) - float(y[i]))**2
-
-    return np.sqrt(sum)
-
-
-def nearest_centroid(centroids, x):
-    distance = [euclidean_distance(mean, x) for mean in centroids]
-    nearest_centroid_index = distance.index(min(distance))
-
-    return nearest_centroid_index
-
-
-def form_cluster(X, k):
-    cluster_centroid = random.sample(X,k)
-    while True:
-        Y = []
-        prev_cluster_centroid = cluster_centroid
-        new_cluster = [[] for i in range(k)]
-        for point in X:
-            nearest_centroid_index = nearest_centroid(cluster_centroid, point)
-            new_cluster[nearest_centroid_index].append(point)
-            Y.append(nearest_centroid_index)
-
-        for i,cluster in enumerate(new_cluster):
-            sum = np.array([0 for j in cluster[0]],dtype='float')
-            for point in cluster:
-                sum += np.array(point, dtype='float')
-
-            cluster_centroid[i] = sum/len(cluster)
-
-        if cluster_centroid == prev_cluster_centroid:
-            break
-
-    return Y
-
-
-
-
-def start(filename, k):
-    data = []
-
+def predict(filename, k):
+    train = []
     with open(filename) as f:
         for line in f:
-            row = line[:-1].split(',')
-            data.append(row)
+            data = line[:-1].split(',')
+            train.append(data)
 
-    Y = np.array(data[0], dtype='int')
-    X = np.array(data[1:]).transpose().tolist()
+    Y = np.array(train[0], dtype='int')
+    X = np.array(train[1:]).transpose().tolist()
+    kmeans = KMeans(n_clusters=k)
+    kmeans.fit(X)
+    Kmeans_labels = kmeans.labels_
 
-    predicted_labels = form_cluster(X, k)
-
-    C = confusion_matrix(Y, predicted_labels)
+    C = confusion_matrix(Y, Kmeans_labels)
     C = C.T
     ind = linear_assignment(-C)
     C_opt = C[:, ind[:, 1]]
     acc_opt = np.trace(C_opt) / np.sum(C_opt)
-    print(acc_opt)
+
+    # with open('output3.txt', 'w') as f:
+    #     f.write(str(C_opt) )
+    # f.close()
+
+    return C_opt,acc_opt
 
 
-if __name__ == '__main__':
-    # filename = 'ATNTFaceImages400.txt'
-    filename = "ATNTFaceImages100.txt"
-    k = 10
-    start(filename, k)
+if __name__ == "__main__":
+    filename = "HandWrittenLetters.txt"
+    # filename = "ATNTFaceImages100.txt"
+    # train = []
+    # with open(filename) as f:
+    #     for line in f:
+    #         data = line[:-1].split(',')
+    #         train.append(data[:100])
+    # data = ""
+    # for i in range(len(train)):
+    #     string = ""
+    #     for j in train[i]:
+    #         string += str(j) + ","
+    #     data += string[:-1] + "\n"
+    #
+    # with open('ATNTFaceImages100.txt', 'w') as f:
+    #     f.write(data)
+    # f.close()
+    k = 26
+    accuracy = predict(filename, k)
+    print("Accuracy:",accuracy)
+
